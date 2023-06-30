bd42e0ff03b4489e53c8c69e81c961c5ff37e981
Akash Yadav <110447309+Withakash@users.noreply.github.com>
2023-06-30 13:25:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ const interval = setInterval(() => {
 
   const current = new Date();
   let hours = current.getHours();
+  const hour = hours;
   const minutes = current.getMinutes();
   const seconds = current.getSeconds();
 
    hours = hours % 12;
+
    hours = hours ? hours : 12;
-  
+   
   countdown.innerHTML = `
     <div data-content="Hours">${hours < 10 ? `0${hours}` : hours}</div>
     <div data-content="Minutes">${minutes < 10 ? `0${minutes}` : minutes}</div>
     <div data-content="Seconds">${seconds < 10 ? `0${seconds}` : seconds}</div>
-    <div data-content="Seconds">${hours > 12 ? 'PM' : 'AM'}</div>
+    <div data-content="Seconds">${hour >= 12 ? 'PM' : 'AM'}</div>
     `
 }, 1000);
