dcff405b2971a9bb5e268b777de9f754f1158fd4
Akash Yadav <110447309+Withakash@users.noreply.github.com>
2023-06-30 11:36:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const countdown = document.querySelector(".countdown");
+
+const interval = setInterval(() => {
+
+  const current = new Date();
+  let hours = current.getHours();
+  const minutes = current.getMinutes();
+  const seconds = current.getSeconds();
+
+   hours = hours % 12;
+
+
+  countdown.innerHTML = `
+    <div data-content="Hours">${hours < 10 ? `0${hours}` : hours}</div>
+    <div data-content="Minutes">${minutes < 10 ? `0${minutes}` : minutes}</div>
+    <div data-content="Seconds">${seconds < 10 ? `0${seconds}` : seconds}</div>
+    <div data-content="Seconds">${hours >= 12 ? 'PM' : 'AM'}</div>
+    `
+}, 1000);
